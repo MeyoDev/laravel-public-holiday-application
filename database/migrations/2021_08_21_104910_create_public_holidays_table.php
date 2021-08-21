72805364d30213048a7f337995ef9ae6f439ad54
Moses <katsande.moses@gmail.com>
2021-08-21 13:01:02 +0200

    Added public holidays migration table.

---
+++ database/migrations/2021_08_21_104910_create_public_holidays_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePublicHolidaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('public_holidays', function (Blueprint $table) {
+            $table->id();
+            $table->string("ph_name");
+            $table->integer("ph_day");
+            $table->integer("ph_month");
+            $table->integer("ph_year");
+            $table->integer("ph_day_of_week");
+            $table->timestamps();
+            
+            $table->primary('id');
+            $table->index(['id', 'ph_name', 'ph_year']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('public_holidays');
+    }
+}
